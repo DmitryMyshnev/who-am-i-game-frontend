4266bfdc3f45f3b44c3e34c3500bfbcd56d80323
Dmitry <dmitrymishnev@gmail.com>
2022-06-29 16:44:59 +0300

    changed proxy link fnd game status constant

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,9 @@
 const WAITING_FOR_PLAYERS =
-  'com.eleks.academy.whoami.core.state.WaitingForPlayers';
+  'WAITING_FOR_PLAYER';
 const SUGGESTING_CHARACTERS =
-  'com.eleks.academy.whoami.core.state.SuggestingCharacters';
+  'SUGGESTING_CHARACTER';
 const PROCESSING_QUESTION =
-  'com.eleks.academy.whoami.core.state.ProcessingQuestion';
+  'PROCESSING_QUESTION';
 const NUMBER_OF_PLAYERS = 4;
 const READY = 'READY';
 const NOT_READY = 'NOT_READY';
