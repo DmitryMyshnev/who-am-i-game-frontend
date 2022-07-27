8ef37ad7029fe88de236058ca6c8b160557fae10
Dmitry <dmitrymishnev@gmail.com>
2022-07-27 23:40:32 +0300

    changed max count player for test

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,7 +4,7 @@ const SUGGESTING_CHARACTERS =
   'com.eleks.academy.whoami.core.state.SuggestingCharacters';
 const PROCESSING_QUESTION =
   'com.eleks.academy.whoami.core.state.ProcessingQuestion';
-const NUMBER_OF_PLAYERS = 4;
+const NUMBER_OF_PLAYERS = 5;
 const READY = 'READY';
 const NOT_READY = 'NOT_READY';
 const ASKING = 'ASKING';
